6317f1963cb791d0f4857835ce8539c9a40a7c0c
rhys powell <rhys.powell2@btinternet.com>
2025-03-13 15:14:27 +0000

    kafka streaming of logs added

--- frontend/functions/logging_functions.py
+++ frontend/functions/logging_functions.py
@@ -1,19 +1,43 @@
 # Import python packages
+from confluent_kafka import Producer
 from typing import Callable
 import streamlit as st
 import datetime
 import logging
 
+
+class KafkaLoggingHandler(logging.Handler):
+    def __init__(self, kafka_broker, topic):
+        super().__init__()
+        self.producer = Producer({'bootstrap.servers': kafka_broker})
+        self.topic = topic
+
+    def emit(self, record):
+        try:
+            log_entry = self.format(record)
+            self.producer.produce(self.topic, log_entry.encode('utf-8'))
+            self.producer.flush()
+        except Exception as e:
+            print(f"Error sending log to Kafka: {e}")
+
+
 def configure_logging() -> logging.Logger:
     '''
     '''
-    # Configure Logger
     logger = logging.getLogger("streamlit_app")
+
     if not logger.hasHandlers():
-        handler = logging.StreamHandler()
+        # Console Handler
+        stream_handler = logging.StreamHandler()
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # Kafka Handler
+        kafka_handler = KafkaLoggingHandler(kafka_broker='localhost:9092', topic='streamlit_logs')
+        kafka_handler.setFormatter(formatter)
+        logger.addHandler(kafka_handler)
+
         logger.setLevel(logging.INFO)
 
     return logger
